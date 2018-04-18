a9f6d2643e4409f70be033be8ed43c9131ae4e86
kurcam23 <34920076+kurcam23@users.noreply.github.com>
2018-04-18 22:04:16 +0200

    testing searchweb

--- hello/SearchWeb.php
+++ hello/SearchWeb.php
@@ -1,16 +1,15 @@
 <?php
 $xml = simplexml_load_file('./XMLProducts/Products.xml');
-$search= htmlspecialchars($_POST["search"]);
-//$search = 'nike';
-$product = $xml->xpath("/RoyalSport/product[contains(description,'{$search}')]/*");
+//$search= htmlspecialchars($_POST["search"]);
+$search = 'nike';
+$product = $xml->xpath("/RoyalSport/category/subcategory/product[contains(description,'{$search}')]");
+print_r($product);
 //@Name='{$search}' OR
-if(count($product) > 0) { // if found
-    if(count($product)>4)
-    {
-        include 'listing.html';
-    }
-    else {
-        include 'detailsPage.html';
-    }
+if(count($product) == 0) { // if found
+    include 'detailsPage.php';
+}
+else{
+    //foreach
+    include 'listing.php';
 }
 ?>
