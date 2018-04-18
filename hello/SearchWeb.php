83e0b8654bdf45ea00e96e47923937879a64f9db
emanabela <eman.abela.17@um.edu.mt>
2018-04-19 00:31:30 +0200

    ugieh fid duda ta karmenu kbira :)

--- hello/SearchWeb.php
+++ hello/SearchWeb.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("auto_detect_line_endings", true);
 $xml = simplexml_load_file('./XMLProducts/Products.xml');
 //$search= htmlspecialchars($_POST["search"]);
 $search = 'nike';
@@ -7,9 +8,8 @@ $product = $xml->xpath("/RoyalSport/category/subcategory/product[contains(descri
 $cid = $xml->xpath("/RoyalSport/category/subcategory/product[contains(description,'{$search}')]/parent::*/parent::*/id")[0];
 $pid = $xml->xpath("/RoyalSport/category/subcategory/product[contains(description,'{$search}')]/id")[0];
 //@Name='{$search}' OR
-if(count($product) <= 1) { // if found
-    // include "detailsPage.php?categoryId=".$cid."&productId=".$pid;
-    include "detailsPage.php?";
+if(count($product) <= 1) {
+    header("Location: detailsPage.php?categoryId=".$cid."&productId=".$pid);
 }
 else{
     //foreach
