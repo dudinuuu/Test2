e3be8872bec06aeb31a2b1a6c6c9e2ca08e9eb4c
kurcam23 <34920076+kurcam23@users.noreply.github.com>
2018-04-20 21:44:09 +0200

    that checkout goodshiiz

--- hello/checkout.php
+++ hello/checkout.php
@@ -30,12 +30,11 @@
 
   <body>
 
-     <div id="navigation"></div>
+    <div id="navigation"></div>
 
     <div class="container">
 
-      <h1 class="mt-4 mb-3">Checkout
-      </h1>
+      <h1 class="mt-4 mb-3">Checkout</h1>
 
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
@@ -44,71 +43,89 @@
         <li class="breadcrumb-item active">Checkout</li>
       </ol>
 
-      <div class="container">
-    <div class="row">
-        <div class="col-xs-12 col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">
-                        Payment Details
-                    </h3>
-                </div>
-                <div class="panel-body">
-                    <form role="form">
-                    <div class="form-group">
-                        <label for="cardNumber">
-                            CARD NUMBER</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="cardNumber" placeholder="Valid Card Number"
-                                required autofocus />
-                            <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-7 col-md-7">
-                            <div class="form-group">
-                                <label for="expityMonth">
-                                    EXPIRY DATE</label>
-                                <div class="col-xs-6 col-lg-6 pl-ziro">
-                                    <input type="text" class="form-control" id="expityMonth" placeholder="MM" required />
-                                </div>
-                                <div class="col-xs-6 col-lg-6 pl-ziro">
-                                    <input type="text" class="form-control" id="expityYear" placeholder="YY" required /></div>
-                            </div>
-                        </div>
-                        <div class="col-xs-5 col-md-5 pull-right">
-                            <div class="form-group">
-                                <label for="cvCode">
-                                    CV CODE</label>
-                                <input type="password" class="form-control" id="cvCode" placeholder="CV" required />
-                            </div>
-                        </div>
-                    </div>
-                    </form>
-                </div>
+    <div class="container">
+
+      <!-- In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
+      <div class="row">
+        <div class="col-lg-8 mb-4">
+          <h3>Enter personal deatils:</h3>
+          <form name="checkout" id="checkoutForm" novalidate>
+            <div class="control-group form-group">
+              <div class="controls">
+                <label>Full Name:</label>
+                <input type="text" class="form-control" id="name"  required ata-validation-required-message="Please enter your name.">
+                <p class="help-block"></p>
+              </div>
             </div>
-            <ul class="nav nav-pills nav-stacked">
-                <li class="active"><a href="#"><span class="badge pull-right"><span class="glyphicon glyphicon-usd">
-                </li>
-            </ul>
-            <br/>
-            <a href="http://www.jquery2dotnet.com" class="btn btn-success btn-lg btn-block" role="button">Checkout and Pay</a>
+            <div class="control-group form-group">
+              <div class="controls">
+                <label>Phone Number:</label>
+                <input type="tel" class="form-control" id="phone"  required data-validation-required-message="Please enter your phone number.">
+              </div>
+            </div>
+            <div class="control-group form-group">
+              <div class="controls">
+                <label>Email Address:</label>
+                <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+              </div>
+            </div>
+            <h2>Payment Details</h2>
+            <div class="control-group form-group">
+              <div class="controls">
+                <label>CARD NUMBER:</label>
+                <div class="input-group">
+                  <input type="cardNumber" class="form-control" id="cardNumber" required data-validation-required-message="Please enter your card number." placeholder="Valid Card Number">
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-7 col-md-7">
+                    <div class="form-group">
+                        <label for="expityMonth">
+                            EXPIRY DATE</label>
+                        <div class="col-xs-6 col-lg-6 pl-ziro">
+                            <input type="text" class="form-control" id="expityMonth" required placeholder="MM"  />
+                        </div>
+                        <div class="col-xs-6 col-lg-6 pl-ziro">
+                            <input type="text" class="form-control" id="expityYear" required placeholder="YY"  /></div>
+                    </div>
+                </div>
+                <div class="col-xs-5 col-md-5 pull-right">
+                    <div class="form-group">
+                        <label for="cvCode">
+                            CV CODE</label>
+                        <input type="password" class="form-control" id="cvCode" required placeholder="CV"  />
+                    </div>
+                </div>
+              </div>
+              <div id="success"></div>
+              <!-- For success/fail messages -->
+              <button type="submit" class="btn btn-primary" id="checkoutButton">Checkout</button>
+            </form>
           </div>
+
+        </div>
+      <!-- /.row -->
       </div>
     </div>
-</div>
 
 
-<div>
-  <p></p>
-</div>
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Royal Sport 2017-2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
 
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<footer class="py-5 bg-dark">
-  <div class="container">
-    <p class="m-0 text-center text-white">Copyright &copy; Royal Sport 2017-2018</p>
-  </div>
-</footer>
+    <!-- Contact form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/checkout.js"></script>
 
   </body>
 
---
+++ hello/mail/checkoutMail.php
@@ -0,0 +1,61 @@
+<?php
+include 'xmlLoader.php';
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$cardNumber = strip_tags(htmlspecialchars($_POST['cardNumber']));
+$expityMonth = strip_tags(htmlspecialchars($_POST['expityMonth']));
+$expityYear = strip_tags(htmlspecialchars($_POST['expityYear']));
+$cvCode = strip_tags(htmlspecialchars($_POST['cvCode']));
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'royal.sport.101@gmail.com';                 // SMTP username
+    $mail->Password = 'royalsport';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    //Recipients
+    $mail->setFrom($email_address, $name);
+    //$mail->addAddress('royal.sport.101@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addAddress('royal.sport.101@gmail.com');               // Name is optional
+    $mail->addReplyTo($email_address, 'Client');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+
+    //Content
+    $mail->isHTML(true);                        // Set email format to HTML
+    $mail->Subject = 'Enquiry';
+    $mail->Body    =  'CardNumber: '.$cardNumber. '<br>Expiry: '.$expityMonth.'/'.$expityYear.'<br>CV: '.$cvCode.'<br><br>Name: '.$name. '<br>Phone number: '.$phone. '<br>Email: '.$email_address;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+?>
