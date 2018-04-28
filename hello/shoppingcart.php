d33ee765c04ef17fe0a8cd6b55929f0e38f40fb2
emanabela <eman.abela.17@um.edu.mt>
2018-04-28 17:25:35 +0200

    some styling in progress

--- hello/shoppingcart.php
+++ hello/shoppingcart.php
@@ -5,14 +5,19 @@ print_r($_SESSION["cart"]);
 
   <!DOCTYPE html>
   <html lang="en">
+  <style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 
   <body>
     <h1> Shopping cart</h1>
 
     <div>
-      <ul id="show-cart">
+      <table id="show-cart">
         <!-- -->
-      </ul>
+      </table>
       <div  >Total Cart: €<span id="total-cart"></span></div>
     </div>
 
@@ -37,17 +42,16 @@ print_r($_SESSION["cart"]);
 
       function displayCart(){
         var cartArray = listCart();
-        var output = "";
+        var output = "<tr><th>Name</th><th>Quantity</th><th>Price</th><th>-</th><th>+</th><th>Delete</th></tr>";
         for(var i in cartArray){
-          output += "<li>"
-          +cartArray[i].id
-          +" "+cartArray[i].name
-          +" "+cartArray[i].quantity
-          +" €"+cartArray[i].price
-          +" <button class='subtract-item' data-id='"+cartArray[i].id+"'>-</button>"
-          +" <button class='add-item' data-id='"+cartArray[i].id+"'>+</button>"
-          +" <button class='delete-item' data-id='"+cartArray[i].id+"'>X</button>"
-          +"</li>"
+          output += "<tr><td>"
+          +cartArray[i].name
+          +"</td><td>"+cartArray[i].quantity
+          +"</td><td>€"+cartArray[i].price
+          +"</td><td><button class='subtract-item' data-id='"+cartArray[i].id+"'>-</button>"
+          +"</td><td><button class='add-item' data-id='"+cartArray[i].id+"'>+</button>"
+          +"</td><td><button class='delete-item' data-id='"+cartArray[i].id+"'>X</button></td>"
+          +"</tr>"
         }
         $("#show-cart").html(output);
         $("#total-cart").html(totalCostCart().toFixed(2));
